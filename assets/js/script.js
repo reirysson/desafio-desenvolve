b6b0200a0c253aa358e2ab4261400d8c4e7d3d60
reirysson <reiryssonjp@gmail.com>
2022-03-22 17:21:38 -0300

    parte do JS funcionando

---
+++ assets/js/script.js
@@ -0,0 +1,61 @@
+//Declaração do botão
+const btn = document.getElementById("botao-enviar");
+const ok = document.getElementById("ok");
+
+//Evento ao clicar o botão enviar
+btn.addEventListener('click', (envent) => {
+    event.preventDefault();
+
+    email = document.getElementById("email").value;
+    mensagem = document.getElementById("mensagem").value;
+    const alerta = document.getElementById("msg_alerta");
+    let cont = 0;
+    //Verificação do e-mail
+    let user = email.substr(0, email.indexOf("@"));
+    let domain = email.substr(email.indexOf("@") + 1);
+    domain = domain.substr(0, domain.indexOf("."))
+    if (
+        //Verificar se o usuário digitou algo
+        (email != "")&&
+        //Verificar se o usuário digitou o @
+        (email.includes("@"))&&
+        //Verificar se o usuário o último caractere é um ponto
+        (email[email.length -1] !== ".")&&
+        //Verificar se o usuário é maior que 32
+        (user.length <= 32)&&
+        //Verificar se o dominío é maior que 16
+        (domain.length <= 16)
+    ){
+        cont += 1;
+    }else {
+        alerta.innerHTML = "Erro no envio: Endereço de e-mail inválido";
+        ok.classList.remove("escondido")
+        ok.classList.add("visivel");
+    }
+    //Verificar se o usuário digitou alguma mensagem
+    if(mensagem == false){
+        alerta.innerHTML = "Erro no envio: insira uma mensagem";
+        ok.classList.remove("escondido");
+        ok.classList.add("visivel");
+    }else{
+        cont += 1;
+    }
+
+    if(cont === 2){
+        alerta.innerHTML = `Obrigado pelo contato ${user}`;
+        ok.classList.remove("escondido");
+        ok.classList.add("visivel");
+    }
+})
+//Botão de ok
+ok.addEventListener('click', (evento) => {
+    evento.preventDefault();
+
+    ok.classList.remove("visivel");
+    ok.classList.add("escondido");
+
+    document.getElementById("msg_alerta").innerHTML = ""
+    document.getElementById("mensagem").value = "";
+    document.getElementById("email").value = "";
+
+})
